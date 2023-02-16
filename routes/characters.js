7b143a6a42ab00544f518f40a9892418bb662d6d
serge mirouse <sergemirouse@MBP-de-serge.lan>
2023-02-16 09:41:41 +0100

    minor correction in charaters.js route

--- routes/characters.js
+++ routes/characters.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/characters", async (req, res) => {
-  let { limit, skip, title } = req.query;
+  let { limit, skip, name } = req.query;
 
   if (!limit) {
     limit = "";
@@ -11,13 +11,13 @@ router.get("/characters", async (req, res) => {
   if (!skip) {
     skip = "";
   }
-  if (!title) {
-    title = "";
+  if (!name) {
+    name = "";
   }
 
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${name}`
     );
     res.status(200).json(response.data);
     console.log(response.data);
